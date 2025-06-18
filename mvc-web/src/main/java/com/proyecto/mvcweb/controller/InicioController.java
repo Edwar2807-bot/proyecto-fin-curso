c6ec3a343fcbf28c59123d4fdfd8260161395885
Edwar2807 <edwar280700@gmail.com>
2025-06-18 15:14:27 -0500

    mvc-web

---
+++ mvc-web/src/main/java/com/proyecto/mvcweb/controller/InicioController.java
@@ -0,0 +1,15 @@
+package com.proyecto.mvcweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InicioController {
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+        model.addAttribute("mensaje", "¡Bienvenido al sistema!");
+        return "index"; // Busca templates/index.html
+    }
+}
---
+++ mvc-web/src/test/java/com/proyecto/mvcweb/MvcWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.mvcweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
